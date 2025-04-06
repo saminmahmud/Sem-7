3d61199896099c521b5bb1ee020ebef0ac939632
saminmahmud <samin.mahmud18@gmail.com>
2025-04-06 22:48:47 +0600

    fibonacciBottomUp

---
+++ Algorithm/10-4/Fibonacci.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    vector<int> dp(n + 1);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    // for(int i = 0; i <dp.size(); i++) {
+    //     cout << dp[i] << " ";
+    // }
+    // cout << endl;
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int result = fibonacci(n);
+
+    cout << result << endl;
+
+    return 0;
+}
